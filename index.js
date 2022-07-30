d1b8b2b08d422e3dcdb824fe48f38941ec4e1317
Jesse B <jessejbarr@gmail.com>
2022-07-30 12:22:58 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,30 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 //var port = process.env.PORT || 443
+const path = require('path');
+const router = express.Router();
 var path = '/example-smart-app';
+
 
 var app = express();
 
-const http = require('http');
-const fs = require('fs');
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('/example-smart-app/index.html').pipe(res)
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
+
+
+//const http = require('http');
+//const fs = require('fs');
+
+//const server = http.createServer((req, res) => {
+ // res.writeHead(200, { 'content-type': 'text/html' })
+  //fs.createReadStream('/example-smart-app/index.html').pipe(res)
+//});
 
 //server.listen(process.env.PORT || 3000)
 // viewed at http://localhost:8080
